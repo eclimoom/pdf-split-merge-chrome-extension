e4d36caae8332b6107f8fe3c1434f715d22fa6bf
hejunji <junjihe@gmail.com>
2025-01-22 11:36:21 +0800

    feat: pdf 合并 基本逻辑完成

--- src/app/features/file-upload/file-upload.component.ts
+++ src/app/features/file-upload/file-upload.component.ts
@@ -15,7 +15,7 @@ export class FileUploadComponent {
   splitList: any[] = [];
 
   // 模式
-  isSplit: boolean = false;
+  isSplit: boolean = true;
 
   constructor() {
     // todo get pdf.worker.js from assets by environment
@@ -37,24 +37,26 @@ export class FileUploadComponent {
 
     let list: any = [];
     for (const file of files) {
+      const arrayBuffer = await file.arrayBuffer();
       if (file.type.includes('pdf')) {
-        const arrayBuffer = await file.arrayBuffer();
         const splitFiles = await this.splitPdf(arrayBuffer);
         splitFiles.forEach((pdf, index) => {
-          const newFile = {
+          const newFile =  {
             name: `${file.name}_${pdf.pageNumber}.pdf`,
             size: pdf.pdfBytes.byteLength,
             type: 'application/pdf',
-            base64: pdf.base64
+            base64: pdf.base64,
+            arrayBuffer: () => file.arrayBuffer()
           };
           list.push(newFile);
         });
       } else if (file.type.startsWith('image/')) {
         // get image base64
         file.base64 = await this.readFileAsBase64(file);
+        // console.log(file.base64)
         list.push(file);
       } else {
-        alert('不支持的文件类型'+file.type);
+        alert('不支持的文件类型' + file.type);
       }
 
     }
@@ -62,16 +64,34 @@ export class FileUploadComponent {
 
 
     this.files = this.files.concat(list);
-    // console.log(this.files)
-
-    // 如果是分割模式，直接分割
-    this.splitPdfAll().then();
   }
 
   readFileAsBase64(file: File): Promise<string> {
     return new Promise((resolve, reject) => {
       const reader = new FileReader();
-      reader.onload = () => resolve(reader.result as string);
+      reader.onload = () => {
+        let base64 = reader.result as string;
+        // to png
+        if (file.type !== 'image/png') {
+          const canvas = document.createElement('canvas');
+          const img = new Image();
+          img.src = base64;
+          img.onload = () => {
+            canvas.width = img.width;
+            canvas.height = img.height;
+            const ctx = canvas.getContext('2d');
+            if (ctx) {
+              ctx.drawImage(img, 0, 0);
+              base64 = canvas.toDataURL('image/png');
+              resolve(base64);
+            } else {
+              reject('canvas error');
+            }
+          };
+        } else {
+          resolve(base64);
+        }
+      };
       reader.onerror = error => reject(error);
       reader.readAsDataURL(file);
     });
@@ -80,12 +100,12 @@ export class FileUploadComponent {
   async processFile(index: number, files: File[]) {
     if (index >= files.length) return;
 
+    console.log(`>>>>>> 拆分pdf ${index + 1} of ${files.length}`);
     const file = files[index];
-    console.log('file', file);
     const arrayBuffer = await file.arrayBuffer();
     const splitFiles = await this.splitPdf(arrayBuffer);
 
-    const name = file.name.replace('.pdf', '');
+    const name = file.name.slice(0, file.name.lastIndexOf('.'));
     splitFiles.forEach((pdf, index) => {
       const newFile = {
         name: `${name}_${pdf.pageNumber}.pdf`,
@@ -117,20 +137,24 @@ export class FileUploadComponent {
       let page: any;
       [page] = await pdfDoc.copyPages(originalPdf, [i]);
       pdfDoc.addPage(page);
-      // console.log(`Page ${i + 1} content:`, page.doc);
+      console.log(`Page ${i + 1} content:`, page.doc);
       // Serialize the PDF to bytes
       const pdfBytes = await pdfDoc.save();
+      console.log('pdfBytes>>>>>>11111', pdfBytes);
       console.log(`Page ${i + 1} extracted, size: ${pdfBytes.byteLength} bytes`);
 
 
-      let pdf = await pdfjsLib.getDocument(pdfBytes).promise;
+      let pdf = await pdfjsLib.getDocument(new Uint8Array(pdfBytes)).promise;
 
       const basePage = await pdf.getPage(1);
-      const viewport = basePage.getViewport({scale: 1});
-      //
+      const view = basePage.view;
+      let originWidth = view[2] - view[0];
+      const scale = 1122 / originWidth;
+      const viewport = basePage.getViewport({scale});
+
       const canvas = document.createElement('canvas');
-      canvas.width = viewport.width;
-      canvas.height = viewport.height;
+      canvas.width = Math.floor(viewport.width);
+      canvas.height = Math.floor(viewport.height);
       const context = canvas.getContext('2d');
 
       if (context) {
@@ -141,10 +165,10 @@ export class FileUploadComponent {
         await basePage.render(renderContext).promise;
       }
       let base64 = canvas.toDataURL('image/png');
-      // console.log('base64====', base64);
+      console.log('base64========', pdfBytes, base64.length);
       splitFiles.push({
         pageNumber: i + 1,
-        pdfBytes,
+        pdfBytes: pdfBytes,
         base64
       });
     }
@@ -198,9 +222,6 @@ export class FileUploadComponent {
     });
   }
 
-  handleModel() {
-    this.isSplit = !this.isSplit;
-  }
 
   listOrderChanged($event: any) {
     console.log('listOrderChanged', $event);
@@ -214,28 +235,57 @@ export class FileUploadComponent {
   async mergeFiles() {
     const pdfDoc = await PDFDocument.create();
 
+    // 获取当前时间 MMDD_HHmm
+    const firstName = this.files[0].name;
+    const name = firstName.slice(0, firstName.lastIndexOf('.'))
+    let fileName = `${name}_合并`;
+
     // const pdfjs = pdfjsLib.getDocument(this.files[0]);
     for (const file of this.files) {
-      // const pdfDoc2 = await PDFDocument.load(file);
-      // const [existingPage] = await pdfDoc.copyPages(pdfDoc2, [0])
-      // pdfDoc.addPage(existingPage)
+      let pngImage = await pdfDoc.embedPng(file.base64);
+      const pngDims = pngImage.scale(1);
+      const page = pdfDoc.addPage();
+      const pageWidth = page.getWidth();
+      const pageHeight = page.getHeight();
+      const aspectRatio = pngDims.width / pngDims.height;
 
-      const page = pdfDoc.addPage()
-      // convert pngbase64 to PDFImage
-      const pdfImage = await pdfDoc.embedPng(file.base64)
-      // draw image to the center of the page
-      page.drawImage(pdfImage, {
+      let width, height;
+      if (pageWidth / aspectRatio <= pageHeight) {
+        width = pageWidth;
+        height = pageWidth / aspectRatio;
+      } else {
+        width = pageHeight * aspectRatio;
+        height = pageHeight;
+      }
+      page.drawImage(pngImage, {
         x: 0,
-        y: 0,
-        width: page.getWidth(),
-        height: page.getHeight(),
-      })
+        y: pageHeight - height,
+        width: width,
+        height: height,
+      });
     }
     const mergedPdfBytes = await pdfDoc.save();
     const blob = new Blob([mergedPdfBytes], {type: 'application/pdf'});
     const link = document.createElement('a');
     link.href = URL.createObjectURL(blob);
-    link.download = 'merged.pdf';
+    link.download = `${fileName}.pdf`;
     link.click();
+  }
+
+
+  handleModel(flag: boolean) {
+    console.log(`handleModel: ${flag}`);
+    this.isSplit = flag;
+    if (flag) {
+      this.splitPdfAll().then();
+    }
+  }
+
+  handleByModel() {
+    if (this.isSplit) {
+      this.splitPdfAll().then();
+    } else {
+      this.mergeFiles().then();
+    }
   }
 }
